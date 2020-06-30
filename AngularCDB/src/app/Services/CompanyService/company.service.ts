c66b25a89c99e75cde4ba6bc09ba51964d59a8b6
PierreLC <plecourtois@excilys.com>
2020-06-30 11:10:38 +0200

    Models + Services created

--- AngularCDB/src/app/Services/CompanyService/company.service.ts
+++ AngularCDB/src/app/Services/CompanyService/company.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Company } from 'src/app/Models/Company/company.model';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CompanyService {
 
-  constructor() { }
+  private databaseUrl = 'MOCK';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getCompanies(): Observable<Company[]> {
+    return this.httpClient.get<Company[]>(this.databaseUrl);
+  }
+
+  createCompany(company: Company): Observable<number> {
+    return this.httpClient.post<number>(this.databaseUrl, company);
+  }
+
+  getCompany(id: number): Observable<Company> {
+    return this.httpClient.get<Company>(`${ this.databaseUrl }/${ id }`);
+  }
+
+  deleteCompany(id: number): Observable<number> {
+    return this.httpClient.delete<number>(`${ this.databaseUrl }/${ id }`)
+  }
+
+  updateCompany(company: Company): Observable<number> {
+    return this.httpClient.put<number>(this.databaseUrl, company);
+  }
 }
--- AngularCDB/src/app/Services/ComputerService/computer.service.ts
+++ AngularCDB/src/app/Services/ComputerService/computer.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Computer } from 'src/app/Models/Computer/computer.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ComputerService {
 
-  constructor() { }
+  private databaseUrl = 'MOCK';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getComputers(): Observable<Computer[]> {
+    return this.httpClient.get<Computer[]>(this.databaseUrl);
+  }
+
+  createComputer(computer: Computer): Observable<number> {
+    return this.httpClient.post<number>(this.databaseUrl, computer);
+  }
+
+  getComputer(id: number): Observable<Computer> {
+    return this.httpClient.get<Computer>(`${ this.databaseUrl }/${ id }`);
+  }
+
+  deleteComputer(id: number): Observable<number> {
+    return this.httpClient.delete<number>(`${ this.databaseUrl }/${ id }`)
+  }
+
+  updateComputer(computer: Computer): Observable<number> {
+    return this.httpClient.put<number>(this.databaseUrl, computer);
+  }
 }
