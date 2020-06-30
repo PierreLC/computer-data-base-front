d9bb066dabc031fb41a96ab193c7728a66505558
Corentin-B <banteignie.c@gmail.com>
2020-06-30 14:57:07 +0200

    Computer-details Complete

--- AngularCDB/src/app/custom-material/custom-material.module.ts
+++ AngularCDB/src/app/custom-material/custom-material.module.ts
@@ -7,6 +7,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 
 const materialModule = [
@@ -17,6 +18,7 @@ const materialModule = [
   MatExpansionModule,
   MatProgressSpinnerModule,
   MatTableModule,
+  MatGridListModule,
 ];
 
 @NgModule({
